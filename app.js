67a702fd74e7e67f2fca8819d2be08649b659927
ajaythopate <ajaythopate999@gmail.com>
2022-09-17 16:26:28 +0530

    feat: added evenlisner on click

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 var userinput = document.querySelector("#user-inpute");
 var outputbutton = document.querySelector("#output-button");
 var transletedoutput = document.querySelector("#transleted-output");
+
+
+function clickHandler() {
+    console.log("print");
+}
+
+
+
+outputbutton.addEventListener("click", clickHandler)
