0aacdb59f1f205ee86ddf3abc1236d4e059b837d
ajaythopate <ajaythopate999@gmail.com>
2022-09-20 21:27:21 +0530

    creted js

--- app.js
+++ app.js
@@ -2,11 +2,26 @@ var userinput = document.querySelector("#user-inpute");
 var outputbutton = document.querySelector("#output-button");
 var transletedoutput = document.querySelector("#transleted-output");
 
+var serverURL = 'https://api.funtranslations.com/translate/pirate.json';
 
-function clickHandler() {
-    console.log("print");
+function getTranslationURL(userinput) {
+    return serverURL + "?" + "text=" + userinput
 }
 
 
+function insertTransletedoutput(transletedText) {
+    return transletedoutput.innerHTML = transletedText;
+}
+function clickHandler() {
+    var userinput = userinput.value;
+    fetch(getTranslationURL(userinput)).then(responce => responce.json()).then(data => {
+        var transletedText = data.contents.transleted;
+        console.log(data)
+        insertTransletedoutput(transletedText)
+    })
+
+
+
+}
 
 outputbutton.addEventListener("click", clickHandler)
